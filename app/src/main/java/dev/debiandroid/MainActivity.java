db42fa7bf3cc07d08f0fee2ddb5a181902432e35
Elisha Hollander <just4now666666@gmail.com>
2026-09-09 23:12:20 +0300

    refactor `MainActivity` (#9)
    - Enable shift key modifier
    - Disable keyboard and keybar until rootfs is installed to avoid null pointer dereference
    - Make insets abide by `statusBars` and `navigationBars`
    - Prefer `Thread` usage instead of `Executor` where possible
    - Use `xterm` instead of default `linux` term
    - Do rescale request detection based on relative location to support half-screen and pop-ups
    - Strip Open With file names to support weird ContentResolvers
    - Copy Open With files in a thread to avoid hanging the process
    - Implement `xterm` key bindings

--- app/src/main/java/dev/debiandroid/MainActivity.java
+++ app/src/main/java/dev/debiandroid/MainActivity.java
@@ -49,6 +49,7 @@ public final class MainActivity extends Activity {
     private float fontSize = 30f;
     private volatile boolean ctrlDown = false;
     private volatile boolean altDown  = false;
+    private volatile boolean shiftDown = false;
 
     @Override
     protected void onCreate(Bundle state) {
@@ -80,8 +81,6 @@ public final class MainActivity extends Activity {
             }
         );
         terminal.setFocusableInTouchMode(true);
-        Client client = new Client();
-        terminal.setTerminalViewClient(client);
         terminal.setOnTouchListener((v, event) -> {
             if (event.getAction() == MotionEvent.ACTION_DOWN) {
                 terminal.post(() -> {
@@ -113,15 +112,16 @@ public final class MainActivity extends Activity {
         addKeyButton(row2, "\u2192", () -> session.write(key('C')), null,                   true,  true);
         keybar.addView(row1, new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 0, 1.0f));
         keybar.addView(row2, new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 0, 1.0f));
-        root.addView(keybar, new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, (int)(70 * getResources().getDisplayMetrics().density)));
-        setContentView(root);
         root.setOnApplyWindowInsetsListener((v, insets) -> {
-            v.setPadding(0, insets.getInsets(WindowInsets.Type.displayCutout()).top, 0, insets.getInsets(WindowInsets.Type.ime()).bottom);
+            int topInsets = insets.getInsets(WindowInsets.Type.statusBars() | WindowInsets.Type.displayCutout()).top;
+            int bottomInsets = insets.getInsets(WindowInsets.Type.navigationBars() | WindowInsets.Type.ime()).bottom;
+            v.setPadding(0, topInsets, 0, bottomInsets);
             return insets;
         });
+        setContentView(root);
         root.requestApplyInsets();
 
-        Executors.newSingleThreadExecutor().execute(() -> {
+        new Thread(() -> {
             File rootfs = new File(getFilesDir(), "rootfs");
             File tmp = new File(rootfs, "tmp");
             File home = new File(rootfs, "root");
@@ -184,6 +184,12 @@ public final class MainActivity extends Activity {
                 try {marker.createNewFile();} catch (Exception e) {}
             }
 
+            Client client = new Client();
+            terminal.setTerminalViewClient(client);
+            runOnUiThread(() -> {
+                root.addView(keybar, new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, (int)(70 * getResources().getDisplayMetrics().density)));
+            });
+
             String[] args = {
                     "proot",
                     "-l", "-0",
@@ -199,7 +205,7 @@ public final class MainActivity extends Activity {
                         "TMPDIR=/tmp",
                         "PREFIX=/usr",
                         "LC_ALL=C.UTF-8",
-                        "TERM=linux",
+                        "TERM=xterm-256color",
                         "DEBIAN_FRONTEND=noninteractive",
                         "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
                         "/bin/bash", "--rcfile", "/root/.bashrc"
@@ -221,12 +227,12 @@ public final class MainActivity extends Activity {
                 terminal.requestFocus();
                 wakeLock.acquire();
             });
-        });
+        }).start();
     }
 
     @Override
     public boolean dispatchTouchEvent(MotionEvent ev) {
-        if ((ev.getRawY() < terminal.getBottom()) || scaleDetector.isInProgress()) {
+        if ((ev.getY() < terminal.getBottom()) || scaleDetector.isInProgress()) {
             scaleDetector.onTouchEvent(ev);
         }
         return super.dispatchTouchEvent(ev);
@@ -255,7 +261,7 @@ public final class MainActivity extends Activity {
         try (Cursor cursor = getContentResolver().query(uri, new String[]{OpenableColumns.DISPLAY_NAME}, null, null, null)) {
             if (cursor != null && cursor.moveToFirst()) {
                 String name = cursor.getString(cursor.getColumnIndexOrThrow(OpenableColumns.DISPLAY_NAME));
-                File destination = new File(getFilesDir(), "rootfs/root/" + name);
+                File destination = new File(getFilesDir(), "rootfs/root/" + new File(name).getName());
                 if (destination.exists()) {
                     new AlertDialog.Builder(this)
                             .setTitle("File already exists")
@@ -264,13 +270,15 @@ public final class MainActivity extends Activity {
                             .setPositiveButton("Overwrite", (dialog, which) -> { destination.delete(); handleOpenIntent(intent); })
                             .show();
                 } else {
-                    try (InputStream in = getContentResolver().openInputStream(uri)) {
-                        Files.copy(in, destination.toPath());
-                    }
+                    new Thread(() -> { 
+                        try (InputStream in = getContentResolver().openInputStream(uri)) {
+                            Files.copy(in, destination.toPath());
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }).start();
                 }
             }
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 
@@ -319,12 +327,11 @@ public final class MainActivity extends Activity {
     }
 
     private String key(char c) {
-        int m = 1 + (altDown ? 2 : 0) + (ctrlDown ? 4 : 0);
-        return (m == 1) ? "\033[" + c : "\033[1;" + m + c;
+        int m = 1 + (shiftDown ? 1 : 0) + (altDown ? 2 : 0) + (ctrlDown ? 4 : 0);
+        return (m == 1) ? "\033O" + c : "\033[1;" + m + c;
     }
 
     private final class Client implements TerminalSessionClient, TerminalViewClient {
-        private volatile boolean shiftDown = false;
         private volatile boolean fnDown = false;
 
         @Override public void onTextChanged(TerminalSession s) {
