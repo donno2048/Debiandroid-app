240b36a22531b02b0958c79aac7fff57c05371d9
Elisha Hollander <just4now666666@gmail.com>
2026-09-16 14:07:26 +0300

    ignore battery optimizations and use one `wakeLock` for all instances (#14)

--- app/src/main/java/dev/debiandroid/ForegroundService.java
+++ app/src/main/java/dev/debiandroid/ForegroundService.java
@@ -4,11 +4,25 @@ import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
+import android.os.Build;
+import android.provider.Settings;
+import android.net.Uri;
+import android.net.wifi.WifiManager;
+import android.net.wifi.WifiManager.WifiLock;
 
 public final class ForegroundService extends Service {
     private static final String CHANNEL = "debiandroid";
+    @SuppressWarnings("deprecation")
+    private static final int WIFI_MODE = Build.VERSION.SDK_INT < 34
+                                            ? WifiManager.WIFI_MODE_FULL_HIGH_PERF
+                                            : WifiManager.WIFI_MODE_FULL_LOW_LATENCY;
+    private static WakeLock wakeLock;
+    private static WifiLock wifiLock;
 
     @Override
     public void onCreate() {
@@ -17,6 +31,23 @@ public final class ForegroundService extends Service {
             new NotificationChannel(CHANNEL, "Debiandroid", NotificationManager.IMPORTANCE_NONE)
         );
         startForeground(1, new Notification.Builder(this, CHANNEL).setSmallIcon(R.mipmap.ic_launcher).build());
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        wakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "Debiandroid:WakeLock");
+        wakeLock.acquire();
+        wifiLock = ((WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE)).createWifiLock(WIFI_MODE, "Debiandroid:WifiLock");
+        wifiLock.acquire();
+        if (!pm.isIgnoringBatteryOptimizations(getPackageName())) {
+            Intent ignoreOptimizations = new Intent(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS, Uri.parse("package:" + getPackageName()));
+            ignoreOptimizations.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(ignoreOptimizations);
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        if (wakeLock != null && wakeLock.isHeld()) wakeLock.release();
+        if (wifiLock != null && wifiLock.isHeld()) wifiLock.release();
+        super.onDestroy();
     }
 
     @Override public int onStartCommand(Intent intent, int flags, int startId) { return START_NOT_STICKY; }
